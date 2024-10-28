34cd1a30dedc3b5b64879560a071cfb32545961e
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2024-10-28 15:41:03 -0100

    ijbbuj

---
+++ enums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+enum Level{
+    low,
+    MEDIAUM,
+    HIGH
+};
+
+int main () {
+
+}
+ /*
+ Array de Inteiros com 20 elementos
+ ciclo for para mostrar apenas os elementos pares
+ usar for
+ */
