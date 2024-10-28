246d58444ea7fe1bf795a09bdfdb70e6ba740de6
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2024-10-28 15:18:17 -0100

    fafa

---
+++ arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+  
+
+
+
+int numerosImpares[10]; //arrays n iniciado
+int  numerosPares[10] = {0,2,4,6,8,10,12,14,116,18}; //arrays inicido
+
+string nomeDosAlunos [4] = {"Joao", "Maria", "José"};
+
+int main () {
+    
+   
+}
