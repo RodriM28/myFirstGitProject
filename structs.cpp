06bfafb1baaa0e093b3f4d713dd34bb9dc0ecc9d
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2024-10-28 15:28:51 -0100

    jsainia

---
+++ structs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct Aluno {
+        string nome;
+        int idade;
+        string turma;
+    };
+
+int main() { 
+
+Aluno xpto676 ={"Joana", 15, "A" };
+    
+}
