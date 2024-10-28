bbd6d241c94c8a9fcb25eb26767f033cbca924cb
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2024-10-28 14:33:55 -0100

    oohohohoh

---
+++ teste.4.5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int x=0; x<=10; x++ )
+    if (x == 5){
+        continue;
+    }
+    cout << "Hello world!:"  << endl;
+
+    //continue (cintinua)
+    //break (para)
+}
---
+++ teste4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+for ( int x=0; x <= 10 ;x+=2) //precisa sempre de 3 para trabalhar (for tem 3 letras)
+  cout << "Hello, world!" << endl;
+}
