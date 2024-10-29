5bacaad0745ca22389777a41774f46d50e4becbf
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2024-10-29 16:51:00 -0100

    bbk

--- trabalho1c++.cpp
+++ trabalho1c++.cpp
@@ -4,8 +4,8 @@
  int main() { 
      
      int numerosPares [20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-     for(int x=0 ; x < 20; x++){
-         if(numerosPares[x] %2 == 0)
+     for(int x=0 ; x < 20; x+=2){
          cout << numerosPares[x] << endl;
      }
+     return 0;
  }
