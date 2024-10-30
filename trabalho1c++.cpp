3cbbcfed5ae494c5854b2515c8aae470ee89f6d2
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2024-10-30 09:03:43 -0100

    onjonj

--- trabalho1c++.cpp
+++ trabalho1c++.cpp
@@ -3,9 +3,9 @@
 
  int main() { 
      
-     int numerosPares [20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-     for(int x=0 ; x < 20; x++){
-         if(numerosPares[x] %2 == 0)
+     int numerosPares [21] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+     for(int x=0 ; x <= 20; x+=2){
+        
          cout << numerosPares[x] << endl;
      }
      return 0;
