8102db17fb4913bab317e36513462e5f90fefbf0
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2024-11-06 10:31:27 -0100

    kkk

---
+++ .vscode/exercicio5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ using namespace std;
+
+ float notas [10] = {20, 10, 2, 3,4,18,20, 13.4,14.6,2};
+
+ int main () { 
+    
+     float Soma =0, media=0;
+     for( int n=0;n<10; n++ ){
+        Soma += notas[n];// += e com fose x= x+y  ( sé )
+
+     }
+     
+
+ 
+return 0;
+ }
---
+++ .vscode/tpc.cpp
@@ -0,0 +1,13 @@
+/* TPC 1
+Um programa que peça uma media em metros 
+E converta para milimetros*/
+
+/*tPC2
+UM PROGRAMA QUE PEÇA O RAIO DE UM CIRCULO
+E apresente o seu perimetro e a sua area */
+
+/* Tpc 
+UM PROGRAMA UE VOS PEÇA O VALOR/HORA DE UM TRABALHADOR,
+O NÚMERO DE HORASTRABALHADAS E APRESENTE O VALOR TOTAS A PAGAR PELAS 
+HORAS TRABALHADAS.E o desconto de 12.5% para o IRS*/
+
