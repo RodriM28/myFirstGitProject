d0b881150c1a1d7939c03e32a108b7ca685cc24d
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2024-11-20 10:32:02 -0100

    nnnnn

---
+++ .vscode/contador.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//vou fazer um contador que começa a contar no numero 80 vai acabar no numero 200 com incrementos de 5 em 5
+
+for( int x= 80; x<=200; x+=5){
+    cout<< x << " " ;
+}
+
+return 0;
+}
