ea8a5b90c21ceca7b903e8c267be636128eca2a4
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2025-02-17 15:31:49 -0100

    gkgkgkhk

---
+++ edsdad.cpp
@@ -0,0 +1,33 @@
+/*
+#include <iostream>
+using namespace std;
+
+int conta(int& x , int& y){
+
+    x = 10;
+    y = 20;
+
+  return x + y;
+}
+// &-aponta para o endereso de memoria
+int main() {
+int a =5;
+int b = 10;
+
+    cout << a <<endl; 
+    cout << conta(a,b) <<endl;
+    cout << a << endl; //como x ta apontar para a memoria e x ta defenido com 10 e o a ta no de parametro é x modificando as variaveis po isso b també é 10poupando memoria
+                        se n tiver o & n altera a variaval e mesmo que & estivese no codigo e na tivese defenido a e b ficava igual
+return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
