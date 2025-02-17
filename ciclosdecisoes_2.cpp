b41a3621d7662a11522cd4041af3b8cb4e8ec0b4
RodrigoM28 <rodrigomatosmartins2024@gmail.com>
2025-02-17 13:56:36 -0100

    kJiopj

---
+++ ciclosdecisoes_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+string frase =   "Ou sabem ou n sabem"; 
+
+int main() {
+
+for(char t : frase) t != ' ' ? cout << "_" : cout << "  ";  
+         //          condição |   verdadde  | falso 
+}
---
+++ ciclosdecisoes_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//struct morseLetter{
+  //  char letra;
+    //string code;
+//};
+
+int main() {
+        /*for(char l : frase){
+        for(moseLatter m: morseAlphabet){
+            l == m.latter ? cout << m.codee << " " : cout << " ";
+            }
+        }*/ 
+ 
+
+}
